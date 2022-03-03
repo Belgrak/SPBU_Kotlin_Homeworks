a62756b2c5322d73df0c4781b356c8f823d6c69e
Glebus <glekar03@mail.ru>
2022-03-03 22:35:37 +0300

    correct mistakes

--- src/main/kotlin/homeworks/homework1/task_2.kt
+++ src/main/kotlin/homeworks/homework1/task_2.kt
@@ -1,24 +1,24 @@
 package homeworks.homework1
 
-fun sieveOfEratosthenes(n: Int): MutableList<Int> {
-    val primeNumbers = (2..n).toMutableList()
-    val tempList = primeNumbers.toList()
-    for (i in tempList) {
-        if (i !in primeNumbers)
-            continue
-        for (j in tempList) {
-            if (j in primeNumbers && i != j && j % i == 0)
-                primeNumbers.remove(j)
-        }
+fun sieveOfEratosthenes(n: Int): List<Int> {
+    require(n >= 0) { "Number must be non-negative, was $n" }
+    val isPrimeNumber = BooleanArray(n + 1) { true }
+    isPrimeNumber[0] = false
+    isPrimeNumber[1] = false
+    for (i in 2..n) {
+        if (isPrimeNumber[i] && i * i <= n)
+            for (j in i * i..n step i)
+                isPrimeNumber[j] = false
     }
-    return primeNumbers
+    return isPrimeNumber.flatMapIndexed { index, b -> if (b) listOf(index) else emptyList() }
 }
 
 fun main() {
     print("Please enter a number: ")
     try {
-        val num = readLine()!!.toIntOrNull() ?: throw TypeCastException("Integer was expected")
-        print("Prime numbers: " + sieveOfEratosthenes(num).joinToString())
+        val num = readLine()?.toIntOrNull() ?: throw TypeCastException("Integer was expected")
+        val result = sieveOfEratosthenes(num)
+        print(if (result.isEmpty()) "There aren't any prime numbers(" else "Prime numbers: " + result.joinToString())
     } catch (e: TypeCastException) {
         print(e.message)
         return
