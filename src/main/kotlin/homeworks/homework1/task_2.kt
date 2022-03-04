509e67a78cbb31c183880381425e89ecdc29e94c
Glebus <glekar03@mail.ru>
2022-03-04 13:34:52 +0300

    correct mistakes v2

--- src/main/kotlin/homeworks/homework1/task_2.kt
+++ src/main/kotlin/homeworks/homework1/task_2.kt
@@ -2,25 +2,37 @@ package homeworks.homework1
 
 fun sieveOfEratosthenes(n: Int): List<Int> {
     require(n >= 0) { "Number must be non-negative, was $n" }
-    if (n == 0) return emptyList()
-    val isPrimeNumber = BooleanArray(n + 1) { true }
+    if (n == 0) {
+        return emptyList()
+    }
+    val isPrimeNumber = BooleanArray(n + 1) { true }.toTypedArray()
     isPrimeNumber[0] = false
     isPrimeNumber[1] = false
     for (i in 2..n) {
-        if (isPrimeNumber[i] && i * i <= n)
-            for (j in i * i..n step i)
+        if (isPrimeNumber[i]) {
+            for (j in (i * i)..n step i) {
                 isPrimeNumber[j] = false
+            }
+        }
     }
-    return isPrimeNumber.flatMapIndexed { index, b -> if (b) listOf(index) else emptyList() }
+    return isPrimeNumber.mapIndexedNotNull { index, b -> if (b) index else null }
 }
 
 fun main() {
     print("Please enter a number: ")
+    val num = readLine()?.toIntOrNull()
+    if (num == null) {
+        print("Integer was expected")
+        return
+    }
     try {
-        val num = readLine()?.toIntOrNull() ?: throw TypeCastException("Integer was expected")
         val result = sieveOfEratosthenes(num)
-        print(if (result.isEmpty()) "There aren't any prime numbers(" else "Prime numbers: " + result.joinToString())
-    } catch (e: TypeCastException) {
+        if (result.isEmpty()) {
+            print("There aren't any prime numbers(")
+        } else {
+            print("Prime numbers: " + result.joinToString())
+        }
+    } catch (e: IllegalArgumentException) {
         print(e.message)
         return
     }
--- src/test/kotlin/homeworks/homework1/SieveOfEratosthenesTest.kt
+++ src/test/kotlin/homeworks/homework1/SieveOfEratosthenesTest.kt
@@ -1,15 +1,16 @@
 package homeworks.homework1
 
-import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertFailsWith
 
 internal class SieveOfEratosthenesTest {
 
     @Test
     fun correctTest() {
-        assertArrayEquals(
-            intArrayOf(
+        assertEquals(
+            listOf(
                 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79,
                 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
                 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311,
@@ -19,17 +20,21 @@ internal class SieveOfEratosthenesTest {
                 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857,
                 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997
             ),
-            sieveOfEratosthenes(1000).toIntArray()
+            sieveOfEratosthenes(1000)
         )
     }
 
     @Test
     fun testWithZero() {
-        assertArrayEquals(intArrayOf(), sieveOfEratosthenes(0).toIntArray())
+        assertEquals(emptyList<Int>(), sieveOfEratosthenes(0))
     }
 
     @Test
     fun testWithNegativeNumber() {
+        assertFailsWith<IllegalArgumentException>(
+            message = "Number must be non-negative, was -1",
+            block = { sieveOfEratosthenes(-1) }
+        )
         assertThrows<IllegalArgumentException> { sieveOfEratosthenes(-1) }
     }
 }
